dd2ff9b2c810cf467d54987928b22abe03be7140
Kyle Cameron <kcameron85@gmail.com>
2020-07-04 12:17:25 -0700

    Creating text field for "Title" Select field

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@ window.onload = function() {
     document.getElementById('name').focus();
 };
 
+//creates "Other" triggered "Job Role Field"
+
+const createJobRoleField = () => {
+    //Selects all the options within the "Title" <select> element
+    const jobOptions = document.querySelectorAll('#title option');
+    
+
+    //creating hidden text field and setting type, id, and placeholder attributes
+    const textField = document.createElement('input');
+    textField.type = 'text';
+    textField.id = 'other-title';
+    textField.setAttribute("placeholder", "Your Job Role");
+
+
+}
+
