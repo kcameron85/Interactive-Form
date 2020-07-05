653b6893893420b368d4b631e6b2f85b230f0bb9
Kyle Cameron <kcameron85@gmail.com>
2020-07-05 14:56:44 -0700

    Hide/show Job Role title

--- js/script.js
+++ js/script.js
@@ -15,15 +15,19 @@ window.onload = function() {
 const jobOptions = document.querySelector('#title');
 //Selects input box that appears when "other" is selected
 const otherInputBox = document.getElementById('other-title');
+const inputLabel = document.querySelector('label[for="other-title"]')
 
 //Sets display property of hidden input element to "none"
 otherInputBox.style.display = "none";
+inputLabel.style.display = "none"
 
 //Listens for the Select option to change then sets display value of hidden text input
 jobOptions.addEventListener('change', (e) => {
     if (e.target.value == 'other') {
         otherInputBox.style.display = "block";
+        inputLabel.style.display = "block"
     } else {
         otherInputBox.style.display = "none";
+        inputLabel.style.display = "none"
     }
 });
